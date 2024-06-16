458dd001a7a706a3c4ab2ac68fc71de922f2869b
kikelkik <kikelkik@corpium.net>
2024-06-16 12:09:13 +0200

    fix lint issue

--- inc/class_plugins.php
+++ inc/class_plugins.php
@@ -126,7 +126,7 @@ class pluginSystem
 			{
 				foreach($hooks as $key => $hook)
 				{
-					if($hook['file'] ?? false)
+					if(!empty($hook['file']))
 					{
 						require_once $hook['file'];
 					}
